6e8451e86e8fc13e453f14f44008a8dda1e21a1d
arwinters <arwinters@nforce-it.nl>
2017-05-11 02:19:04 +0200

    Update helloworld.ps1

--- files/helloworld.ps1
+++ files/helloworld.ps1
@@ -1,5 +1,5 @@
 # Filename: helloworld.ps1
 Write-Host
-Write-Host 'Hello World!' | Write-Output c:\windows\temp\hello-ansi.txt
-Write-Host "Good-bye World! `n" | Write-Output c:\windows\temp\goodbye-ansi.txt
+Write-Host 'Hello World!' | Out-File c:\windows\temp\hello-ansi.txt
+Write-Host "Good-bye World! `n" | Out-File c:\windows\temp\goodbye-ansi.txt
 # end of script
